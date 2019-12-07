3e329d7fd5f74ed76d22efb3befce93b87f50fba
Fernando Santos <fern@ndo.io>
2019-12-06 21:05:58 -0300

    EOY Splash

--- src/app/utils/modal.js
+++ src/app/utils/modal.js
@@ -12,20 +12,39 @@ export class Modal {
         end_date: "",
         background_desktop: "",
         background_mobile: "",
+        background_credit: "",
+        align: "left",
         debug: false
       },
       options
     );
+    if (!this.shouldRun()) {
+      // If we're not between the running dates, get out
+      return false;
+    }
     const markup = `
-    <div class="desktop_bg" style="background-image: url('${this.options.background_desktop}');"></div>
-    <div class="mobile_bg" style="background-image: url('${this.options.background_mobile}');"></div>
+    <div class="desktop_bg" style="background-image: url('${
+      this.options.background_desktop
+    }');"></div>
+    <div class="mobile_bg" style="background-image: url('${
+      this.options.background_mobile
+    }');"></div>
     <div class="ifawSplash-container">
         <a href="#" class="button-close"></a>
         <span href="#" class="logo">IFAW</span>
-        <div class="container">
+        <div class="container ${this.options.align}">
         <h1 class="title">${this.options.title}</h1>
         <p>${this.options.paragraph}</p>
-        <a class="cta" href="${this.options.cta_link}">${this.options.cta_label}</a>
+        <a class="cta" href="${this.options.cta_link}">${
+      this.options.cta_label
+    }</a>
+        ${
+          this.options.background_credit
+            ? "<p class='credits'>Photo © " +
+              this.options.background_credit +
+              "</p>"
+            : ""
+        }
         </div>
     </div>`;
     let overlay = document.createElement("div");
@@ -43,19 +62,23 @@ export class Modal {
     document.body.appendChild(overlay);
     window.setTimeout(this.open.bind(this), 5000); //Delay before opening splash screen
   }
+  shouldRun() {
+    let start = new Date(this.options.start_date);
+    let end = new Date(this.options.end_date);
+    start.setHours(0, 0, 0);
+    end.setHours(23, 59, 59);
+    let now = new Date();
+    if (this.options.debug) {
+      console.log(start);
+      console.log(end);
+      console.log(now);
+    }
+    return now >= start || now <= end;
+  }
   open() {
     let hideSplash = crumbs.get("hideSplash"); // Get cookie
     if (!hideSplash || this.options.debug) {
-      const now = new Date();
-      let startDate = new Date(this.options.start_date);
-      startDate = startDate.setHours(0, 0, 0); // Starts on the first second of the Start Date
-      let endDate = new Date(this.options.end_date);
-      endDate = endDate.setHours(11, 59, 59); // End on the last second of the End Date
-
-      // Right now is on or after the Start Date AND on or before the End Date
-      if (startDate <= now && now <= endDate) {
-        this.overlay.classList.remove("is-hidden");
-      }
+      this.overlay.classList.remove("is-hidden");
     }
   }
   close(e) {
